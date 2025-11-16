d049ad3668f8e355d88dfa68311f4deead8c0e48
Shanreed25 <tralainereed@gmail.com>
2025-11-16 14:11:45 -0800

    added utility functions

---
+++ Demo3/README.md
@@ -0,0 +1,19 @@
+
+
+## Files & Folders
+
+- **`reader.py`** : reader process
+- **`writer.py`** : writer process
+
+### UI Folder
+- **`reader_layout.py`** : layout for reader
+- **`writer_layout.py`** : layout for writer
+- **`common_layout.py`** : layout for header and footer for reader and writer
+- **`account_layout.py`** : layout for accounts table
+- **`transaction_layout.py`** : layout for transaction table
+
+
+### Utils Folder
+- **`account.py`** : utility functions for account management
+- **`transaction.py`** : utility functions for handling transactions
+
--- Demo3/utils/account.py
+++ Demo3/utils/account.py
@@ -5,8 +5,10 @@ def add_new_account(state, console):
    """
     Adds a new account to the shared state
    """
-
+    # Get new account details from user
    new_account_details = get_new_account_details(console)
+
+   # Add new account to state
    state["accounts"].append(new_account_details)
    return state
 
@@ -15,6 +17,8 @@ def get_accounts(state):
     Get the list of accounts from the shared state
     return: list - A list of account dictionaries
     """
+
+    # Get list of accounts
     accounts = state.get("accounts", [])
     return accounts
 
@@ -23,6 +27,8 @@ def get_account_names(accounts):
     Get a list of account names from the shared state
     return: list - A list of account names
     """
+
+    # Get list of account names
     accounts_names = [account['name'] for account in accounts]
 
     return accounts_names
